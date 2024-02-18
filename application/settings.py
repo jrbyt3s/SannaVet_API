23f2fc5be92c514b3b6ecd2ab985b73b65511110
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-18 08:54:00 -0500

    deploy: add pip install whetenoise v5

--- application/settings.py
+++ application/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
