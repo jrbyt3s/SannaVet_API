5bc18e414fe9b568941a6f06c820d03412272cd8
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-18 03:46:53 -0500

    deploy: add pip install whetenoise v2

--- application/settings.py
+++ application/settings.py
@@ -50,13 +50,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware"
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',    
+    'django.contrib.sessions.middleware.SessionMiddleware',    
+    'django.middleware.common.CommonMiddleware',    
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',    
     'django.contrib.messages.middleware.MessageMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware"
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
