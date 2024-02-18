82a35057cfdd4a8f80256713faa0d1f2adbfedf4
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-18 02:24:46 -0500

    deploy: add minetypes

--- alive/views.py
+++ alive/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import generics
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import ViewSerializer
 
 # Create your views here.
 
 class AliveView(generics.GenericAPIView):
+    serializer_class = ViewSerializer
     http_method_names = ['get']
 
     def get(self, request):
--- application/settings.py
+++ application/settings.py
@@ -13,11 +13,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+mimetypes.add_type("text/css", ".css", True)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
