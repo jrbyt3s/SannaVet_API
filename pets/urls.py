6baf11bf6c9d023b5a64d32061690253d20bae0d
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-18 20:00:31 -0500

    petById: se inicia la implementación de los métodos get, patch, delete by ID - v1

--- pets/urls.py
+++ pets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PetView
+from .views import PetView, PetGetByIDView
 
 urlpatterns = [
-    path('', PetView.as_view(), name='list_create')
+    path('', PetView.as_view(), name='list_create'),
+    path('<int:id>/',PetGetByIDView.as_view(), name='read_update_delete' )
 ]
