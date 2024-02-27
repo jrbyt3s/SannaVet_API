039f6192278fea8042a89dcf2823451fdee1940b
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-26 19:12:47 -0500

    incrementando el tiempo de expiración del access_token

--- application/settings.py
+++ application/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
 # Configuración SimpleJWT
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
 
