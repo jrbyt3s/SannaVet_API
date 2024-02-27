caad643cdc79e550fb37c4308ab1b27b16557f6b
jrbyt3s <josee.ramirezi@gmail.com>
2024-02-26 20:20:03 -0500

    add get clients by id

--- clients/urls.py
+++ clients/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ClienteDataView
+from .views import ClienteDataView, ClienteDataByIDView
 
 urlpatterns = [
     path('', ClienteDataView.as_view(), name='list_create'),
-    
+    path('<int:id>/',ClienteDataByIDView.as_view(), name ='read' )
 ]
--- clients/views.py
+++ clients/views.py
@@ -2,9 +2,11 @@ from django.http import JsonResponse
 from django.views import View
 from users.models import UserModel
 from pets.models import PetModel
+from rest_framework.viewsets import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, parsers, permissions
+from django.shortcuts import get_object_or_404
 from .serializers import ClientSerializer
 from drf_yasg.utils import swagger_auto_schema
 
@@ -29,3 +31,21 @@ class ClienteDataView(APIView):
             return Response({'status':'nok'}, status=status.HTTP_404_NOT_FOUND)
     
         return Response(serilizer.data, status=status.HTTP_200_OK)
+    
+
+class ClienteDataByIDView(generics.GenericAPIView):
+    serializer_class = ClientSerializer
+    http_method_names = ['get']
+    queryset = UserModel.objects
+    #parser_classes = [parsers.MultiPartParser]
+    permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_summary='Endpoint para lista la data por id',
+        operation_description='Este servicio retorna toda la dat relacionada a un usuario',
+        
+    )
+    def get(self, _, id):
+        record = get_object_or_404(self.queryset, pk=id, is_active=True)
+        serializer = self.serializer_class(record, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
